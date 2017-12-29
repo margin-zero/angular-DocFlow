3bd2c08eb63d2628dc38582b47c43598b1e478c6
Tomek <margin-zero@atcplanet.com>
2017-12-29 20:18:53 +0100

    admin.component

---
+++ app/components/admin/admin.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { BackendApiService } from '../../services/backend-api/backend-api.service';
+
+@Component({
+  selector: 'dcf-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private backendApiService: BackendApiService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/']);
+  }
+}
