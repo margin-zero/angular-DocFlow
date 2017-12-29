fa4506a74c2651c1a82ff6db1d42a68584ca486e
Tomek <margin-zero@atcplanet.com>
2017-12-29 18:38:19 +0100

    datatypes

---
+++ app/datatypes/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    id: number;
+    username: string;
+    password: string;
+    full_name: string;
+    phone_number: string;
+    email_address: string;
+    additional_info: string;
+    expiration_date: string;
+    is_active: boolean;
+    is_user: boolean;
+    is_admin: boolean;
+}
