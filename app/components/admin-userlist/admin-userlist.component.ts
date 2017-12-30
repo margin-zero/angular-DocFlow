df60e182d3685d26d5edc4f17da73bf4fb353117
Tomek <margin-zero@atcplanet.com>
2017-12-30 23:11:19 +0100

    admin-userlist.component

---
+++ app/components/admin-userlist/admin-userlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dcf-admin-userlist',
+  templateUrl: './admin-userlist.component.html',
+  styleUrls: ['./admin-userlist.component.css']
+})
+export class AdminUserlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
