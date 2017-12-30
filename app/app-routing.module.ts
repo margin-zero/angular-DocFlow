38a9d9e2fcaeaaf1e9de4dad0f7453521f108328
Tomek <margin-zero@atcplanet.com>
2017-12-30 23:08:21 +0100

    app-routing.module

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -2,14 +2,30 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent }   from './components/login/login.component';
+
 import { AdminComponent } from './components/admin/admin.component';
+import { AdminUserlistComponent } from './components/admin-userlist/admin-userlist.component';
+import { AdminPathlistComponent } from './components/admin-pathlist/admin-pathlist.component';
+import { AdminGrouplistComponent } from './components/admin-grouplist/admin-grouplist.component';
+
+
 import { UserComponent } from './components/user/user.component';
+
+import { AuthenticationService } from './services/authentication/authentication.service';
+import { IsAdminGuard, IsUserGuard } from './services/router-guards/router-guards.service';
 
 const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login',  component: LoginComponent },
-    { path: 'admin', component: AdminComponent },
-    { path: 'user', component: UserComponent },
+    { path: 'admin', component: AdminComponent, canActivate: [IsAdminGuard],
+        children: [
+          { path: 'userlist', component: AdminUserlistComponent },
+          { path: 'pathlist', component: AdminPathlistComponent },
+          { path: 'grouplist', component: AdminGrouplistComponent }
+        ]
+    },
+
+    { path: 'user', component: UserComponent, canActivate: [IsUserGuard] },
     { path: '**', redirectTo: 'login', pathMatch: 'full' },
   ];
 
