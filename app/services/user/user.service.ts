02b47ac5154a3f25400fa9f65667b63ab4c5444e
Tomek <margin-zero@atcplanet.com>
2018-01-02 19:07:57 +0100

    user.service

---
+++ app/services/user/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// DATATYPES
+import { User } from '../../datatypes/user';
+
+// CONSTANTS
+import { API_URL } from '../../constants/global-constants';
+
+
+@Injectable()
+export class UserService {
+
+  constructor() { }
+
+}
