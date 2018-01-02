8c07d0737cb73de44865db2c1ba6148ef8204d1b
Tomek <margin-zero@atcplanet.com>
2018-01-02 19:06:59 +0100

    app.module

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,31 +7,39 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 import { LoginComponent } from './components/login/login.component';
+
 import { AdminComponent } from './components/admin/admin.component';
+import { AdminUserlistComponent } from './components/admin-userlist/admin-userlist.component';
+import { AdminGrouplistComponent } from './components/admin-grouplist/admin-grouplist.component';
+import { AdminPathlistComponent } from './components/admin-pathlist/admin-pathlist.component';
+
 import { UserComponent } from './components/user/user.component';
 
 
 import { AppRoutingModule } from './app-routing.module';
-import { UserListComponent } from './user-list/user-list.component';
 
 import { AuthenticationService } from './services/authentication/authentication.service';
 import { BackendApiService } from './services/backend-api/backend-api.service';
+import { UserService } from './services/user/user.service';
+
 import { IsAdminGuard, IsUserGuard } from './services/router-guards/router-guards.service';
-import { AdminUserlistComponent } from './components/admin-userlist/admin-userlist.component';
-import { AdminGrouplistComponent } from './components/admin-grouplist/admin-grouplist.component';
-import { AdminPathlistComponent } from './components/admin-pathlist/admin-pathlist.component';
+import { AdminHomeComponent } from './components/admin-home/admin-home.component';
+import { AdminUserEditComponent } from './components/admin-user-edit/admin-user-edit.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    UserListComponent,
     AdminComponent,
     UserComponent,
     AdminUserlistComponent,
     AdminGrouplistComponent,
-    AdminPathlistComponent
+    AdminPathlistComponent,
+    AdminHomeComponent,
+    AdminUserEditComponent
   ],
   imports: [
     BrowserModule,
@@ -39,7 +47,12 @@ import { AdminPathlistComponent } from './components/admin-pathlist/admin-pathli
     FormsModule,
     HttpClientModule
   ],
-  providers: [AuthenticationService, BackendApiService, IsAdminGuard, IsUserGuard],
+  providers: [
+    AuthenticationService,
+    BackendApiService,
+    UserService,
+    IsAdminGuard,
+    IsUserGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
