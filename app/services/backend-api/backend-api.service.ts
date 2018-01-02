bccc6a15854d3f6feeb3a8fc871dec7eae68a284
Tomek <margin-zero@atcplanet.com>
2018-01-02 19:08:09 +0100

    backend-api.service

--- app/services/backend-api/backend-api.service.ts
+++ app/services/backend-api/backend-api.service.ts
@@ -3,6 +3,11 @@ import { HttpClient } from '@angular/common/http';
 
 // DATATYPES
 import { User } from '../../datatypes/user';
+import { Role } from '../../datatypes/role';
+import { Path } from '../../datatypes/path';
+
+import { ResponseUser } from '../../datatypes/response-classes';
+
 
 // CONSTANTS
 import { API_URL } from '../../constants/global-constants';
@@ -27,16 +32,71 @@ export class BackendApiService {
   }
 
 
-
   login(username: string, password: string): Promise<User> {
 
     const URL = API_URL + 'login/' + username + '/' + this.hashUserPassword(password);
 
-    return this.http.get<User>(URL)
+    return this.http.get<ResponseUser>(URL)
         .toPromise()
-        .then(apiResponse => apiResponse as User)
+        .then(apiResponse => apiResponse.data[0] as User)
         .catch(this.handleError);
   }
+
+
+// -----------------------------------------------------------------------------------------------
+//          USER API
+// -----------------------------------------------------------------------------------------------
+
+getUsers(): Promise<User[]> {
+
+    const URL = API_URL + 'users';
+
+    return this.http.get<ResponseUser>(URL)
+        .toPromise()
+        .then(apiResponse => apiResponse.data as User[])
+        .catch(this.handleError);
+  }
+
+getUser(userId: number): Promise<User> {
+
+    const URL = API_URL + 'user/' + userId;
+
+    return this.http.get<ResponseUser>(URL)
+        .toPromise()
+        .then(apiResponse => apiResponse.data[0] as User)
+        .catch(this.handleError);
+  }
+
+
+
+// -----------------------------------------------------------------------------------------------
+//          ROLE API
+// -----------------------------------------------------------------------------------------------
+
+getRoles(): Promise<Role[]> {
+
+  const URL = API_URL + 'roles';
+
+  return this.http.get<Role[]>(URL)
+      .toPromise()
+      .then(apiResponse => apiResponse as Role[])
+      .catch(this.handleError);
+}
+
+
+// -----------------------------------------------------------------------------------------------
+//          PATH API
+// -----------------------------------------------------------------------------------------------
+
+getPaths(): Promise<Path[]> {
+
+  const URL = API_URL + 'paths';
+
+  return this.http.get<Path[]>(URL)
+      .toPromise()
+      .then(apiResponse => apiResponse as Path[])
+      .catch(this.handleError);
+}
 
 
 }
