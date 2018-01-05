dbefa7f34d98fd09301a89a90ee356fd5896f46b
Tomek <margin-zero@atcplanet.com>
2018-01-05 17:50:38 +0100

    admin-user-edit.component

--- app/components/admin-user-edit/admin-user-edit.component.ts
+++ app/components/admin-user-edit/admin-user-edit.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+
+import { Observable } from 'rxjs/Observable';
 
 import { User } from '../../datatypes/user';
 
 import { BackendApiService } from '../../services/backend-api/backend-api.service';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
-import { PACKAGE_ROOT_URL } from '@angular/core/src/application_tokens';
+import { ComponentCanDeactivate } from '../../services/router-guards/router-guards.service';
+
+// import { PACKAGE_ROOT_URL } from '@angular/core/src/application_tokens';
 
 @Component({
   selector: 'dcf-admin-user-edit',
   templateUrl: './admin-user-edit.component.html',
   styleUrls: ['./admin-user-edit.component.css']
 })
-export class AdminUserEditComponent implements OnInit, OnDestroy {
+export class AdminUserEditComponent implements OnInit, OnDestroy, ComponentCanDeactivate {
 
   id: number;
   user: User;
   oldUser: User;
   private sub: any;
   loggedUser: boolean;
+  responseMessage: string;
 
+  
   constructor(
     private route: ActivatedRoute,
     private location: Location,
@@ -31,6 +37,7 @@ export class AdminUserEditComponent implements OnInit, OnDestroy {
   ngOnInit() {
 
     this.loggedUser = false;
+    this.responseMessage = null;
 
     this.sub = this.route.params.subscribe(params => {
       this.id = +params['userId'];
@@ -49,6 +56,18 @@ export class AdminUserEditComponent implements OnInit, OnDestroy {
     this.sub.unsubscribe();
   }
 
+
+  // @HostListener allows us to also guard against browser refresh, close, etc.
+  @HostListener('window:beforeunload')
+  canDeactivate(): Observable<boolean> | boolean {
+    // insert logic to check if there are pending changes here;
+    // returning true will navigate without confirmation
+    // returning false will show a confirm dialog before navigating away
+    return !this.isChanged();
+  }
+
+
+
   isChanged(): boolean {
     return (JSON.stringify(this.user) !== JSON.stringify(this.oldUser) );
   }
@@ -56,7 +75,15 @@ export class AdminUserEditComponent implements OnInit, OnDestroy {
   handleClick() {
 
     this.backendApiService.updateUser(this.user)
-    .then(apiResponse => this.location.back());
+    .then(apiResponse => {
+      if (apiResponse.status === 'OK') {
+        this.oldUser = Object.assign({}, this.user);
+        this.responseMessage = null;
+        this.location.back();
+      } else {
+        this.responseMessage = apiResponse.message;
+      }
+    });
 
   }
 }
