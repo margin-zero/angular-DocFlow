c156d7d4400fbe10a23966ee967bede6ade65853
Tomek <margin-zero@atcplanet.com>
2018-01-05 17:52:17 +0100

    router-guards.service

--- app/services/router-guards/router-guards.service.ts
+++ app/services/router-guards/router-guards.service.ts
@@ -1,6 +1,9 @@
 import { Injectable, OnInit } from '@angular/core';
-import { CanActivate }    from '@angular/router';
+import { CanActivate, CanDeactivate }    from '@angular/router';
+
 import {Router} from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
 
 import { AuthenticationService } from '../../services/authentication/authentication.service';
 
@@ -67,4 +70,25 @@ export class IsUserGuard implements CanActivate {
     }
   }
 }
+
+
+// ------------------------------ PendingChangesGuard ------------------------------------
+
+
+export interface ComponentCanDeactivate {
+  canDeactivate: () => boolean | Observable<boolean>;
+}
+
+export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate> {
+  canDeactivate(component: ComponentCanDeactivate): boolean | Observable<boolean> {
+    // if there are no pending changes, just allow deactivation; else confirm first
+    return component.canDeactivate() ?
+      true :
+      // NOTE: this warning message will only be shown when navigating elsewhere within your angular app;
+      // when navigating away from your angular app, the browser will show a generic warning message
+      // see http://stackoverflow.com/a/42207299/7307355
+      confirm('UWAGA: Nie zapisano wprowadzonych zmian. Wybierz ANULUJ aby powrócić i zapisać zmiany, lub OK aby zrezygnować z zapisu.');
+  }
+}
+
 
