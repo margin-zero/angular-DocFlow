3768db4136da2cee0fea9a7d6e812f16e501d2de
Tomek <margin-zero@atcplanet.com>
2018-01-07 22:40:25 +0100

    app.module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,37 +1,50 @@
+// @angular imports ----------------------------------------------------------------
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 
+// app-component import ------------------------------------------------------------
 import { AppComponent } from './app.component';
 
+
+// component imports ---------------------------------------------------------------
 import { LoginComponent } from './components/login/login.component';
 
 import { AdminComponent } from './components/admin/admin.component';
-import { AdminUserlistComponent } from './components/admin-userlist/admin-userlist.component';
-import { AdminGrouplistComponent } from './components/admin-grouplist/admin-grouplist.component';
-import { AdminPathlistComponent } from './components/admin-pathlist/admin-pathlist.component';
-
-import { UserComponent } from './components/user/user.component';
-
-
-import { AppRoutingModule } from './app-routing.module';
-
-import { AuthenticationService } from './services/authentication/authentication.service';
-import { BackendApiService } from './services/backend-api/backend-api.service';
-import { UserService } from './services/user/user.service';
-
-import { IsAdminGuard, IsUserGuard } from './services/router-guards/router-guards.service';
-import { PendingChangesGuard } from './services/router-guards/router-guards.service';
-
 import { AdminHomeComponent } from './components/admin-home/admin-home.component';
+
+import { AdminUserlistComponent } from './components/admin-userlist/admin-userlist.component';
 import { AdminUserEditComponent } from './components/admin-user-edit/admin-user-edit.component';
 import { AdminUserViewComponent } from './components/admin-user-view/admin-user-view.component';
 import { AdminUserResetpasswordComponent } from './components/admin-user-resetpassword/admin-user-resetpassword.component';
 import { AdminUserNewComponent } from './components/admin-user-new/admin-user-new.component';
 
+import { AdminGrouplistComponent } from './components/admin-grouplist/admin-grouplist.component';
+import { AdminPathlistComponent } from './components/admin-pathlist/admin-pathlist.component';
 
+
+import { UserComponent } from './components/user/user.component';
+
+
+// routing import ---------------------------------------------------------------------
+import { AppRoutingModule } from './app-routing.module';
+
+
+// routing guards imports (services) --------------------------------------------------
+import { IsAdminGuard, IsUserGuard } from './services/router-guards/router-guards.service';
+import { PendingChangesGuard } from './services/router-guards/router-guards.service';
+
+
+// services imports --------------------------------------------------------------------
+import { AuthenticationService } from './services/authentication/authentication.service';
+import { BackendApiService } from './services/backend-api/backend-api.service';
+import { UserService } from './services/user/user.service';
+
+
+// directive imports --------------------------------------------------------------------
+import { EqualValidatorDirective } from './directives/equal-validator.directive';
 
 
 @NgModule({
@@ -47,7 +60,8 @@ import { AdminUserNewComponent } from './components/admin-user-new/admin-user-ne
     AdminUserEditComponent,
     AdminUserViewComponent,
     AdminUserResetpasswordComponent,
-    AdminUserNewComponent
+    AdminUserNewComponent,
+    EqualValidatorDirective
   ],
   imports: [
     BrowserModule,
