76aaae8ce23028bb145f56e04714b52649ea5453
Tomek <margin-zero@atcplanet.com>
2018-01-08 20:59:39 +0100

    form-model-classes

--- app/datatypes/form-model-classes.ts
+++ app/datatypes/form-model-classes.ts
@@ -1,4 +1,10 @@
+import { User } from './user';
+
 export class FormModelResetPassword {
     password: string;
     confirmPassword: string;
+}
+
+export class FormModelEditUser extends User {
+// for now we have FormModelEditUser === User
 }
