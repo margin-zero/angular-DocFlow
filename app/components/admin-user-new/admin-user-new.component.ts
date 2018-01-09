db70ebbc373b4856b50ce8af4ea7ef6541ed0b7f
Tomek <margin-zero@atcplanet.com>
2018-01-09 19:56:40 +0100

    admin-user-new.component

--- app/components/admin-user-new/admin-user-new.component.ts
+++ app/components/admin-user-new/admin-user-new.component.ts
@@ -5,6 +5,7 @@ import { Location } from '@angular/common';
 import { User } from '../../datatypes/user';
 
 import { BackendApiService } from '../../services/backend-api/backend-api.service';
+import { FormModelEditUser } from '../../datatypes/form-model-classes';
 
 @Component({
   selector: 'dcf-admin-user-new',
@@ -16,27 +17,27 @@ export class AdminUserNewComponent implements OnInit {
   user: User;
   users: User[];
   responseMessage = null;
-
+  formModel: FormModelEditUser;
 
   constructor(
     private backendApiService: BackendApiService,
     private location: Location,
-    private currentRoute: ActivatedRoute,
+    private route: ActivatedRoute,
     private router: Router
   ) { }
 
   ngOnInit() {
-    this.user = new User;
-    this.user.id = 1;
-    this.user.username = '';
-    this.user.password = '';
-    this.user.is_active = 'FALSE';
-    this.user.is_admin = 'FALSE';
-    this.user.is_user = 'FALSE';
-    this.user.full_name = '';
-    this.user.additional_info = '';
-    this.user.email_address = '';
-    this.user.phone_number = '';
+    this.formModel = new FormModelEditUser;
+    this.formModel.id = 1;
+    this.formModel.username = '';
+    this.formModel.password = '';
+    this.formModel.is_active = 'FALSE';
+    this.formModel.is_admin = 'FALSE';
+    this.formModel.is_user = 'FALSE';
+    this.formModel.full_name = '';
+    this.formModel.additional_info = '';
+    this.formModel.email_address = '';
+    this.formModel.phone_number = '';
     this.getUsers();
   }
 
@@ -46,9 +47,9 @@ export class AdminUserNewComponent implements OnInit {
     .then(users => this.users = users);
   }
 
-  isUsernameValid(): boolean {
+  isUsernameValid(username: string): boolean {
 
-    const username = this.user.username.trim();
+    username = username.trim();
     let returnValue = true;
 
     if ( username.length < 1 ) { returnValue = false; }
@@ -60,14 +61,13 @@ export class AdminUserNewComponent implements OnInit {
     return returnValue;
   }
 
-  isPasswordValid(): boolean {
-    return (this.user.password.length > 0);
-  }
+
 
   isChanged(): boolean {
     return true;
   }
 
+  /*
   handleClick() {
     if (this.isUsernameValid() && this.isPasswordValid()) {
 
@@ -87,7 +87,31 @@ export class AdminUserNewComponent implements OnInit {
       } else {
         this.responseMessage = 'Hasło nie może być puste. Wprowadź hasło użytkownika.';
       }
-      
+
     }
+  }
+*/
+
+
+  newUser(model: FormModelEditUser, isValid: boolean) {
+
+    if (!isValid) { return; }
+    if (!this.isUsernameValid(model.username)) { return; }
+
+    this.backendApiService.createUser(model)
+      .then(apiResponse => {
+        if (apiResponse.status === 'OK') {
+          this.responseMessage = null;
+          this.router.navigate(['/admin/user/']);
+        } else {
+          this.responseMessage = apiResponse.message;
+        }
+      });
+
+  }
+
+  handleCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+
   }
 }
