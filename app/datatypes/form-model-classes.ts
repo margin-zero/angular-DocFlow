284948d6411b57f5897cf26a3c2c06896ee9490b
Tomek <margin-zero@atcplanet.com>
2018-01-14 20:54:38 +0100

    form-model-classes

--- app/datatypes/form-model-classes.ts
+++ app/datatypes/form-model-classes.ts
@@ -7,6 +7,19 @@ export class FormModelResetPassword {
 
 export class FormModelEditUser extends User {
 // for now we have FormModelEditUser === User
+    constructor() {
+        super();
+        this.additional_info  = '';
+        this.email_address = '';
+        this.full_name = '';
+        this.id = 0;
+        this.is_active = 'FALSE';
+        this.is_admin = 'FALSE';
+        this.is_user = 'FALSE';
+        this.password = '';
+        this.phone_number = '';
+        this.username = '';
+    }
 }
 
 export class FormModelLogin {
