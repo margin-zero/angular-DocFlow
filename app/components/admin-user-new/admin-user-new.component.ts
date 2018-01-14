ba075dc242bf6765d9560016d4ee2067ae2401fe
Tomek <margin-zero@atcplanet.com>
2018-01-14 20:53:40 +0100

    admin-user-new.component

--- app/components/admin-user-new/admin-user-new.component.ts
+++ app/components/admin-user-new/admin-user-new.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { Observable } from 'rxjs/Observable';
 
 import { User } from '../../datatypes/user';
+import { FormModelEditUser } from '../../datatypes/form-model-classes';
 
 import { BackendApiService } from '../../services/backend-api/backend-api.service';
-import { FormModelEditUser } from '../../datatypes/form-model-classes';
+
+import { ComponentSubscriptionManager } from '../../common-classes/component-subscription-manager.class';
+import { UiAdminFormButtonConfiguration, UiAdminHeaderConfiguration } from '../../datatypes/ui-element-classes';
+
 
 
 @Component({
@@ -17,73 +21,70 @@ import { FormModelEditUser } from '../../datatypes/form-model-classes';
 })
 export class AdminUserNewComponent implements OnInit {
 
-  user: User;
-  users: User[];
-  responseMessage = null;
-  formModel: FormModelEditUser;
-  formModelOld: FormModelEditUser;
+  @ViewChild('f') form: any;
 
-  componentHeader = 'nowy-użytkownik';
-  componentSubheader = 'wprowadzanie nowego użytkownika';
+  users: User[];
+  responseMessage: string = null;
+
+  formModel: FormModelEditUser = new FormModelEditUser();
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
+
+  formButtonConfiguration: UiAdminFormButtonConfiguration = new UiAdminFormButtonConfiguration({});
+
+  headerConfiguration: UiAdminHeaderConfiguration = new UiAdminHeaderConfiguration( {
+    headerText: 'nowy-użytkownik',
+    subheaderText: 'wprowadzanie nowego użytkownika'
+    } );
 
 
   constructor(
     private backendApiService: BackendApiService,
     private location: Location,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private subscriptionManager: ComponentSubscriptionManager
   ) { }
 
 
   ngOnInit() {
-    this.formModel = new FormModelEditUser;
-    this.formModel.id = 1;
-    this.formModel.username = '';
-    this.formModel.password = '';
-    this.formModel.is_active = 'FALSE';
-    this.formModel.is_admin = 'FALSE';
-    this.formModel.is_user = 'FALSE';
-    this.formModel.full_name = '';
-    this.formModel.additional_info = '';
-    this.formModel.email_address = '';
-    this.formModel.phone_number = '';
-    this.getUsers();
-
-    this.formModelOld = Object.assign({}, this.formModel);
-  }
-
-
-  getUsers(): void {
     this.backendApiService
-    .getUsers()
-    .then(users => this.users = users);
+      .getUsers()
+      .then(users => this.users = users);
+
+    this.subscriptionManager.add(
+      this.form.statusChanges.subscribe(value => {
+        this.formButtonConfiguration.submit.disabled = (value !== 'VALID') || !this.isChanged();
+      })
+    );
   }
 
 
-  // @HostListener Pozwala na wykrycie opuszczenia danej lokalizacji nie tylko w efekcie zmiany nawigacji ale
-  //               również poprzez zamknięcie okna, wpisanie innego adresu itp.
+
+  // @HostListener detects navigating out of your current location via router but also
+  //               by closing browser's window, typing in new url etc.
   @HostListener('window:beforeunload')
   canDeactivate(): Observable<boolean> | boolean {
-    // return true - pozwala nawigować bez potwierdzenia
-    // return false - pokazuje dialog potwierdzenia przez nawigacją
+    // return true - change location without user confirmation
+    // return false - shows "OK/Cancel" dialog before navigation
     return !this.isChanged();
   }
 
 
   isChanged(): boolean {
-    return (JSON.stringify(this.formModel) !== JSON.stringify(this.formModelOld) );
+    const emptyForm: FormModelEditUser = new FormModelEditUser();
+    return (JSON.stringify(this.formModel) !== JSON.stringify(emptyForm) );
+    // return true;
   }
 
 
-  newUser(model: FormModelEditUser, isValid: boolean) {
+  saveForm(formData: FormModelEditUser, isValid: boolean) {
 
     if (!isValid) { return; }
 
-    this.backendApiService.createUser(model)
+    this.backendApiService.createUser(formData)
       .then(apiResponse => {
         if (apiResponse.status === 'OK') {
-          this.formModel = null;
-          this.formModelOld = null;
+          this.formModel = new FormModelEditUser();
           this.responseMessage = null;
           this.backendApiService.refreshUsersObservable();
           this.router.navigate(['/admin/user/']);
@@ -91,11 +92,6 @@ export class AdminUserNewComponent implements OnInit {
           this.responseMessage = apiResponse.message;
         }
       });
-  }
-
-
-  handleCancel() {
-    this.location.back();
   }
 
 }
