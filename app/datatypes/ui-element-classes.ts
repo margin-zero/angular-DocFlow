ed994a19e2b92ac07b26964b17f4c40a38c52b88
Tomek <margin-zero@atcplanet.com>
2018-01-14 20:54:54 +0100

    ui-element-classes

--- app/datatypes/ui-element-classes.ts
+++ app/datatypes/ui-element-classes.ts
@@ -1,24 +1,64 @@
-// UiAdminFormButtonConfiguration
-// klasa definiuje typ danych dla konfiguracji komponentu: ui-admin-form-button
-// klasa opisuje trzy przyciski: reset, cancel, submit, każdy z nich możemy w pewnym stopniu konfigurować
-// *.value - jest to napis na odpowiednim przycisku
-// cancel.goBack - określa, czy przycisk ma działać jako "idź wstecz"
-// cancel.navigate - jeśli goBack === false, to przycisk bierze tą wartość jako parametr dla router.navigate
-// cancel.isRelative - jeśli goBack === false, to isRelative określa, czy router.navigate przyjmie wartość relativeTo (aktualna ścieżka)
-// submit.disabled - określa, czy przycisk submit ma być wyświetlany w stanie "disabled"
+// UiAdminFormButtonConfiguration class
+// -------------------------------------
+// Class defines datatype for ui-admin-form-button component's configuration.
+// Class describes properties of three buttons: 'reset', 'cancel' and 'submit' each of them can be configured.
+// *.value - is value of 'value' property of each button
+// cancel.goBack, cancel.navigate and cancel.isRelative describe 'cancel' button behavior.
+// cancel.goBack === true means that 'cancel' button is just 'go back' button, if cancel.goBack is set to 'true'
+// then cancel.navigate and cancel.isRelative are irrelevant.
+// if cancel.goBack is set to 'false' then cancel.navigate and cancel.isRelative are parameters for router
+// describing routing after clicking 'cancel' button.
+// cancel.navigate is path parameter for router.navigate
+// cancel.isRelative set to 'true' means that cancel.navigate parameter will be set relative to current route
+// submit.disabled is used for setting state of 'submit' button to 'disabled' (submit.disabled=true) or
+// 'not disabled' (submit.disabled=false)
 
 export class UiAdminFormButtonConfiguration {
     reset: { value: string };
     cancel: { value: string, goBack: true, navigate: string, isRelative: boolean };
     submit: { value: string, disabled: boolean};
 
+    constructor( params: any) {
+
+        // first we set default values to every parameter
+        this.reset = { value: 'resetuj'};
+        this.cancel = {
+            value: 'anuluj',
+            goBack: true,
+            navigate: null,
+            isRelative: false
+        };
+        this.submit = {
+            value: 'zatwierdź',
+            disabled: true
+        };
+
+        // then we update parameters with sent values
+        const paramArray = Object.keys(params);
+
+        for ( let i = 0; i < paramArray.length; i++ ) {
+            const subparamArray = Object.keys(params[paramArray[i]]);
+            for ( let j = 0; j < subparamArray.length; j++ ) {
+                this[paramArray[i]][subparamArray[j]] = params[paramArray[i]][subparamArray[j]];
+            }
+        }
+    }
 }
 
 
-export function UiAdminFormButtonConfigurationFactory(): UiAdminFormButtonConfiguration {
-    return ({
-        reset: { value: 'resetuj'},
-        cancel: { value: 'anuluj', goBack: true, navigate: null, isRelative: false},
-        submit: { value: 'zatwierdź', disabled: true}
-    });
+
+export class UiAdminHeaderConfiguration {
+    headerText: string;
+    subheaderText: string;
+
+    constructor(params: any) {
+
+        this.headerText = '';
+        this.subheaderText = '';
+
+        const paramArray = Object.keys(params);
+        for ( let i = 0; i < paramArray.length; i++) {
+            this[paramArray[i]] = params[paramArray[i]];
+        }
+    }
 }
