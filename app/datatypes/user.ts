c93c04cea8b32271a028f3cb5fbf3d52f8247d52
Tomek <margin-zero@atcplanet.com>
2018-01-14 20:55:02 +0100

    users

--- app/datatypes/user.ts
+++ app/datatypes/user.ts
@@ -9,4 +9,16 @@ export class User {
     is_active: string;
     is_user: string;
     is_admin: string;
+    constructor() {
+        this.id = 0;
+        this.username = '';
+        this.password = '';
+        this.full_name = '';
+        this.phone_number = '';
+        this.email_address = '';
+        this.additional_info = '';
+        this.is_active = 'FALSE';
+        this.is_user = 'FALSE';
+        this.is_admin = 'FALSE';
+    }
 }
