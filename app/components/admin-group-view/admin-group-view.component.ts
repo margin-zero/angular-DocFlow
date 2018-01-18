6638a1a27b8f5d48706f596aed445fdb17f28b62
Tomek <margin-zero@atcplanet.com>
2018-01-18 18:52:22 +0100

    admin-group-view.component

--- app/components/admin-group-view/admin-group-view.component.ts
+++ app/components/admin-group-view/admin-group-view.component.ts
@@ -5,7 +5,6 @@ import { Group } from '../../datatypes/group';
 import { User } from '../../datatypes/user';
 
 import { BackendApiService } from '../../services/backend-api/backend-api.service';
-import { AuthenticationService } from '../../services/authentication/authentication.service';
 
 import { ComponentSubscriptionManager } from '../../common-classes/component-subscription-manager.class';
 import { UiAdminHeaderConfiguration } from '../../datatypes/ui-element-classes';
@@ -21,13 +20,12 @@ export class AdminGroupViewComponent implements OnInit {
   group: Group;
   users: User[] = [];
 
-  headerConfiguration = new UiAdminHeaderConfiguration({ subheaderText: 'użytkownicy w grupie'});
+  headerConfiguration = new UiAdminHeaderConfiguration({ subheaderText: 'grupa użytkowników'});
 
   constructor(
     private currentRoute: ActivatedRoute,
     private router: Router,
     private backendApiService: BackendApiService,
-    private authenticationService: AuthenticationService,
     private subscriptionManager: ComponentSubscriptionManager
   ) { }
 
