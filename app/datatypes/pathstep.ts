7a4a3dc60d30f89f4ba05e847630fe350e458294
Tomek <margin-zero@atcplanet.com>
2018-01-20 21:41:51 +0100

    pathstep

--- app/datatypes/pathstep.ts
+++ app/datatypes/pathstep.ts
@@ -1,12 +1,12 @@
 export class PathStep {
     id: number;
     path_id: number;
-    name: string;
+    action_id: number;
     step_order: number;
     constructor() {
         this.id = 0;
         this.path_id = 0;
-        this.name = '';
+        this.action_id = 0;
         this.step_order = 0;
     }
 }
