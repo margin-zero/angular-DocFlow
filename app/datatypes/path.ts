d508fca5c181a634a0ab45bec789dbaa2fc77dbc
Tomek <margin-zero@atcplanet.com>
2018-01-25 16:10:16 +0100

    path

--- app/datatypes/path.ts
+++ app/datatypes/path.ts
@@ -2,9 +2,11 @@ export class Path {
     id: number;
     name: string;
     info: string;
+    can_receive: string;
     constructor() {
         this.id = 0;
         this.name = '';
         this.info = '';
+        this.can_receive = 'FALSE';
     }
 }
