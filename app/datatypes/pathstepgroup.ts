17477f04d8b0ad173a70930247c911381478c01b
Tomek <margin-zero@atcplanet.com>
2018-02-12 19:57:12 +0100

    pathstepgroup

---
+++ app/datatypes/pathstepgroup.ts
@@ -0,0 +1,10 @@
+export class PathStepGroup {
+    id: number;
+    name: string;
+    pathstep_id: number;
+    constructor() {
+        this.id = 0;
+        this.name = '';
+        this.pathstep_id = 0;
+    }
+}
