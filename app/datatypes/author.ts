be9d02aa12ba9bc0a61700f466d82d10a586b660
Tomek <margin-zero@atcplanet.com>
2018-03-06 19:10:45 +0100

    author

---
+++ app/datatypes/author.ts
@@ -0,0 +1,18 @@
+export class Author {
+    id: number;
+    name: string;
+    full_name: string;
+    address: string;
+    phone_number: string;
+    email_address: string;
+    additional_info: string;
+    constructor() {
+        this.id = 0;
+        this.name = '';
+        this.full_name = '';
+        this.address = '';
+        this.phone_number = '';
+        this.email_address = '';
+        this.additional_info = '';
+    }
+}
