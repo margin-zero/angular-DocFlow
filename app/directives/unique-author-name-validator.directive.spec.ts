b69fd31bf14e3ac00cce154b9cd4550635819bfa
Tomek <margin-zero@atcplanet.com>
2018-03-07 16:17:32 +0100

    unique-author-name-validator.directive

---
+++ app/directives/unique-author-name-validator.directive.spec.ts
@@ -0,0 +1,8 @@
+import { UniqueAuthorNameValidatorDirective } from './unique-author-name-validator.directive';
+
+describe('UniqueAuthorNameValidatorDirective', () => {
+  it('should create an instance', () => {
+    const directive = new UniqueAuthorNameValidatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ app/directives/unique-author-name-validator.directive.ts
@@ -0,0 +1,51 @@
+import { Directive, OnInit } from '@angular/core';
+import { NG_VALIDATORS, FormControl, Validator } from '@angular/forms';
+
+import { BackendApiService } from '../services/backend-api/backend-api.service';
+
+@Directive({
+  selector: '[dcfUniqueAuthorNameValidator],[validateUniqueAuthorName][ngModel],[uniqueAuthorNameValidator][ngModel]',
+  providers: [
+    { provide: NG_VALIDATORS, useExisting: UniqueAuthorNameValidatorDirective, multi: true }
+  ]
+})
+export class UniqueAuthorNameValidatorDirective implements Validator, OnInit {
+
+  authornames: string[];
+
+  constructor(
+    private backendApiService: BackendApiService
+  ) {}
+
+  ngOnInit() {
+    this.authornames = [];
+
+    this.backendApiService.getAuthors()
+      .then(authors => {
+        this.authornames = authors.map( author => author.name );
+      });
+  }
+
+
+
+  validate(control: FormControl): {[key: string]: any} {
+
+    let isValid = true;
+
+    if ( control.value ) {
+      for (let i = 0; i < this.authornames.length; i++) {
+        if ( this.authornames[i] === control.value ) {
+          isValid = false;
+        }
+      }
+    }
+
+    if (isValid) {
+      return null;
+    } else {
+      return { validateUniqueAuthorName: 'validateUniqueAuthorName' };
+    }
+  }
+
+
+}
