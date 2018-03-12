52c382ec5609a619297782c1db65eecfc3754748
Tomek <margin-zero@atcplanet.com>
2018-03-12 20:18:28 +0100

    user-home.component

--- app/components/user-home/user-home.component.ts
+++ app/components/user-home/user-home.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { User } from '../../datatypes/user';
+import { Group } from '../../datatypes/group';
+import { EntryPathStepGroup } from '../../datatypes/entrypathstepgroup';
+import { BackendApiService } from '../../services/backend-api/backend-api.service';
+import { AuthenticationService } from '../../services/authentication/authentication.service';
+
+import { ComponentSubscriptionManager } from '../../common-classes/component-subscription-manager.class';
+// import { UiAdminHeaderConfiguration } from '../../datatypes/ui-element-classes';
+
 
 @Component({
   selector: 'dcf-user-home',
   templateUrl: './user-home.component.html',
-  styleUrls: ['./user-home.component.css']
+  styleUrls: ['./user-home.component.css'],
+  providers: [ ComponentSubscriptionManager ],
 })
 export class UserHomeComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  groups: Group[] = [];
+  entryPathStepGroups: EntryPathStepGroup[] = [];
+
+  // headerConfiguration = new UiAdminHeaderConfiguration({ subheaderText: 'dane użytkownika'});
+
+  constructor(
+    private currentRoute: ActivatedRoute,
+    private router: Router,
+    private backendApiService: BackendApiService,
+    private authenticationService: AuthenticationService,
+    private subscriptionManager: ComponentSubscriptionManager
+  ) { }
+
 
   ngOnInit() {
-  }
 
+    this.user = this.authenticationService.getUser();
+
+    this.backendApiService.getUserGroups(this.user.id)
+      .then(groups  => {
+          this.groups = groups;
+      });
+
+    this.backendApiService.getEntryPathStepGroups()
+      .then(entryPathStepGroups => {
+          this.entryPathStepGroups = entryPathStepGroups;
+      });
+
+  }
 }
