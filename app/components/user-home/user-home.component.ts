2687b3603dbe2368c432eb6e6a6db8184d17edcf
Tomek <margin-zero@atcplanet.com>
2018-03-13 16:27:39 +0100

    user-home.component

--- app/components/user-home/user-home.component.ts
+++ app/components/user-home/user-home.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { User } from '../../datatypes/user';
 import { Group } from '../../datatypes/group';
+import { Path } from '../../datatypes/path';
+
 import { EntryPathStepGroup } from '../../datatypes/entrypathstepgroup';
 import { BackendApiService } from '../../services/backend-api/backend-api.service';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
@@ -22,6 +24,7 @@ export class UserHomeComponent implements OnInit {
   user: User;
   groups: Group[] = [];
   entryPathStepGroups: EntryPathStepGroup[] = [];
+  paths: Path[] = [];
 
   // headerConfiguration = new UiAdminHeaderConfiguration({ subheaderText: 'dane użytkownika'});
 
@@ -48,5 +51,21 @@ export class UserHomeComponent implements OnInit {
           this.entryPathStepGroups = entryPathStepGroups;
       });
 
+    this.backendApiService.getPaths()
+      .then( paths => {
+          this.paths = paths;
+      });
+
+  }
+
+  canEnterNewDocument(pathId: number): boolean {
+    for (let i = 0 ; i < this.entryPathStepGroups.length; i++) {
+      if (this.entryPathStepGroups[i].path_id === pathId) {
+        for (let j = 0; j < this.groups.length; j++) {
+          if ( this.groups[j].id === this.entryPathStepGroups[i].group_id) { return true; }
+        }
+      }
+    }
+    return false;
   }
 }
