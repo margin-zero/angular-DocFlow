2d7e8b74ca6769cdb8f5a5a218b12efd1cab70c4
Tomek <margin-zero@atcplanet.com>
2018-03-18 21:24:11 +0100

    user-home.component

--- app/components/user-home/user-home.component.ts
+++ app/components/user-home/user-home.component.ts
@@ -4,22 +4,36 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { BackendApiService } from '../../services/backend-api/backend-api.service';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
 
+import { ComponentSubscriptionManager } from '../../common-classes/component-subscription-manager.class';
+
+import { Document } from '../../datatypes/document';
 
 @Component({
   selector: 'dcf-user-home',
   templateUrl: './user-home.component.html',
-  styleUrls: ['./user-home.component.css']
+  styleUrls: ['./user-home.component.css'],
+  providers: [ ComponentSubscriptionManager ]
 })
 export class UserHomeComponent implements OnInit {
 
+  documentsNotReady: Document[];
 
   constructor(
     private currentRoute: ActivatedRoute,
     private router: Router,
     private backendApiService: BackendApiService,
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private subscriptionManager: ComponentSubscriptionManager
   ) { }
 
 
-  ngOnInit() { }
+  ngOnInit() {
+
+    this.subscriptionManager.add(
+      this.backendApiService.getDocumentsNotReadyObservable().subscribe( documents => {
+        this.documentsNotReady = documents;
+      })
+    );
+
+   }
 }
