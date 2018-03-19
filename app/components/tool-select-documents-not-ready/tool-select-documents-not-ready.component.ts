8b008e7e37667e99acd95ed47095262cb0eed2e4
Tomek <margin-zero@atcplanet.com>
2018-03-19 22:35:34 +0100

    tool-select-documents-not-ready.component

--- app/components/tool-select-documents-not-ready/tool-select-documents-not-ready.component.ts
+++ app/components/tool-select-documents-not-ready/tool-select-documents-not-ready.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { BackendApiService } from '../../services/backend-api/backend-api.service';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
 
 import { ComponentSubscriptionManager } from '../../common-classes/component-subscription-manager.class';
+import { UiAdminHeaderConfiguration } from '../../datatypes/ui-element-classes';
 
 import { Document } from '../../datatypes/document';
 
@@ -14,9 +15,17 @@ import { Document } from '../../datatypes/document';
 })
 export class ToolSelectDocumentsNotReadyComponent implements OnInit {
 
+  @Output() onSelectDocument = new EventEmitter<Document>();
+
   documentsNotReady: Document[];
 
-  page = 1;
+  page: number;
+  totalPages: number;
+
+  headerConfiguration = new UiAdminHeaderConfiguration( { headerText: 'Wybierz dokument' } );
+  pageArray = [];
+
+  documentsPerPage = 5;
 
   constructor(
     private backendApiService: BackendApiService,
@@ -25,14 +34,43 @@ export class ToolSelectDocumentsNotReadyComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.page = 1;
+    this.totalPages = 0;
 
     this.subscriptionManager.add(
       this.backendApiService.getDocumentsNotReadyObservable().subscribe( documents => {
         this.documentsNotReady = documents;
+
+        this.setTotalPages(documents.length);
+        this.setPageArray();
       })
     );
 
     this.backendApiService.refreshDocumentsNotReadyObservable(this.authenticationService.getUser().id);
+  }
+
+
+  setTotalPages(elementCount: number) {
+    this.totalPages = Math.floor(elementCount / this.documentsPerPage );
+    if ((elementCount % this.documentsPerPage) > 0) { this.totalPages++ ; }
+  }
+
+
+  setPageArray() {
+      this.pageArray = [];
+      for (let i = 1; i <= this.totalPages; i++ ) {
+        this.pageArray.push(i);
+      }
+  }
+
+
+  setPage(pageNumber: number) {
+    this.page = pageNumber;
+  }
+
+
+  selectDocument(document: Document) {
+    this.onSelectDocument.emit(document);
   }
 
 }
