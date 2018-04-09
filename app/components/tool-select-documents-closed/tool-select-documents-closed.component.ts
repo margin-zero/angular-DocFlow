4e7fd891fac39788fda4048938c9561c4fb00c29
Tomek <margin-zero@atcplanet.com>
2018-04-09 22:53:44 +0200

    tool-select-documents-closed.component

--- app/components/tool-select-documents-closed/tool-select-documents-closed.component.ts
+++ app/components/tool-select-documents-closed/tool-select-documents-closed.component.ts
@@ -2,11 +2,15 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { BackendApiService } from '../../services/backend-api/backend-api.service';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { GlobalFunctionsService } from '../../services/global-functions/global-functions.service';
 
 import { ComponentSubscriptionManager } from '../../common-classes/component-subscription-manager.class';
 import { UiAdminHeaderConfiguration } from '../../datatypes/ui-element-classes';
 
 import { Document } from '../../datatypes/document';
+import { Author } from '../../datatypes/author';
+import { Path } from '../../datatypes/path';
+
 import { FormModelDocumentSearch } from '../../datatypes/form-model-classes';
 
 @Component({
@@ -28,15 +32,25 @@ export class ToolSelectDocumentsClosedComponent implements OnInit {
   headerConfiguration = new UiAdminHeaderConfiguration( { headerText: 'Wybierz dokument' } );
   pageArray = [];
 
+  showToolSelectAuthor = false;
+  selectedAuthor: Author = null;
+
+  showToolSelectPath = false;
+  selectedPath: Path = null;
+
+
   constructor(
     private backendApiService: BackendApiService,
     private authenticationService: AuthenticationService,
-    private subscriptionManager: ComponentSubscriptionManager
+    private subscriptionManager: ComponentSubscriptionManager,
+    private globalFunctions: GlobalFunctionsService
   ) { }
 
   ngOnInit() {
     this.page = 1;
     this.totalPages = 0;
+    this.formModel.input_date_end = this.globalFunctions.getCurrentDateStr();
+    this.formModel.date_by_author_end = this.globalFunctions.getCurrentDateStr();
 
     this.subscriptionManager.add(
       this.backendApiService.getDocumentsAssignedObservable().subscribe( documents => {
@@ -75,5 +89,41 @@ export class ToolSelectDocumentsClosedComponent implements OnInit {
   selectDocument(document: Document) {
     this.onSelectDocument.emit(document);
   }
+
+  toggleShowToolSelectAuthor() {
+    this.showToolSelectAuthor = !this.showToolSelectAuthor;
+  }
+
+  onSelectAuthor(author: Author) {
+    this.selectedAuthor = author;
+    this.showToolSelectAuthor = false;
+
+    this.formModel.author_id = author.id;
+  }
+
+
+  toggleShowToolSelectPath() {
+    this.showToolSelectPath = !this.showToolSelectPath;
+  }
+
+  onSelectPath(path: Path) {
+    this.selectedPath = path;
+    this.showToolSelectPath = false;
+
+    this.formModel.path_id = path.id;
+  }
+
+
+  clearSelectedPath() {
+    this.selectedPath = null;
+    this.formModel.path_id = 0;
+  }
+
+  clearSelectedAuthor() {
+    this.selectedAuthor = null;
+    this.formModel.author_id = 0;
+  }
+
+
 
 }
