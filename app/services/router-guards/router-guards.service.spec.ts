35fb8d53fc390cf1483038eb27efc85b4a31fdda
Tomek <margin-zero@atcplanet.com>
2018-04-09 20:05:47 +0200

    router-guards.service.spec

--- app/services/router-guards/router-guards.service.spec.ts
+++ app/services/router-guards/router-guards.service.spec.ts
@@ -1,15 +1,55 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { RouterGuardsService } from './router-guards.service';
+import { IsAdminGuard, IsUserGuard, IsAuthenticatedGuard, PendingChangesGuard } from './router-guards.service';
 
-describe('RouterGuardsService', () => {
+describe('RouterGuardsService-IsAdminGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [RouterGuardsService]
+      providers: [IsAdminGuard]
     });
   });
 
-  it('should be created', inject([RouterGuardsService], (service: RouterGuardsService) => {
+  it('should be created', inject([IsAdminGuard], (service: IsAdminGuard) => {
+    expect(service).toBeTruthy();
+  }));
+});
+
+
+describe('RouterGuardsService-IsUserGuard', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [IsUserGuard]
+    });
+  });
+
+  it('should be created', inject([IsUserGuard], (service: IsUserGuard) => {
+    expect(service).toBeTruthy();
+  }));
+});
+
+
+describe('RouterGuardsService-IsAuthenticatedGuard', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [IsAuthenticatedGuard]
+    });
+  });
+
+  it('should be created', inject([IsAuthenticatedGuard], (service: IsAuthenticatedGuard) => {
+    expect(service).toBeTruthy();
+  }));
+});
+
+
+
+describe('RouterGuardsService-PendingChangesGuard', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [PendingChangesGuard]
+    });
+  });
+
+  it('should be created', inject([PendingChangesGuard], (service: PendingChangesGuard) => {
     expect(service).toBeTruthy();
   }));
 });
