471be8c9883e5f284ffcb3b5ac3d3207c2bf1c31
Tomek <margin-zero@atcplanet.com>
2018-04-09 20:03:12 +0200

    unique-action-name-validator.directive

--- app/directives/unique-action-name-validator.directive.spec.ts
+++ app/directives/unique-action-name-validator.directive.spec.ts
@@ -1,8 +1,15 @@
 import { UniqueActionNameValidatorDirective } from './unique-action-name-validator.directive';
+import { SimpleChange } from '@angular/core';
+import { BackendApiService } from '../services/backend-api/backend-api.service';
+import { componentFactoryName } from '@angular/compiler';
 
+/*
 describe('UniqueActionNameValidatorDirective', () => {
   it('should create an instance', () => {
     const directive = new UniqueActionNameValidatorDirective();
     expect(directive).toBeTruthy();
   });
 });
+*/
+
+
